55118aa72329775d7b209e56bfeaa0fbc0090a0d
ozanerdogan90 <ozaerdogan@stu.okan.edu.tr>
2019-11-30 19:12:58 +0100

    cart component is finalized

--- src/app/home/cart/cart.component.ts
+++ src/app/home/cart/cart.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ShoppingQuery } from '../state/shopping/shopping.query';
-import { map } from 'rxjs/operators';
+import { ProductService } from '../product/state/product.service';
+import { zip } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
     templateUrl: 'cart.component.html',
     selector: 'app-cart',
-    styleUrls: ['cart.component.scss']
+    styleUrls: ['cart.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CartComponent {
-
-    constructor(private shoppingCartQuery: ShoppingQuery) {
+    constructor(private shoppingCartQuery: ShoppingQuery, private productService: ProductService) {
     }
-    cart$ = this.shoppingCartQuery.cart$.pipe(map(cart => cart));
+    totalPrice;
+    prd$ = zip(
+        this.productService.getAllProducts(),
+        this.shoppingCartQuery.products$
+    ).pipe(map(([products, cartItems]) => {
+        let data = [];
+        cartItems.forEach(cartItem => {
+            const product = products.find(x => x.id === cartItem.id);
+            data.push({ ...product, ...cartItem });
+        });
+        return data;
+    }),
+        tap(data => {
+            this.totalPrice = data
+                .map(x => x.quantity * x.price)
+                .reduce((a, b) => a + b, 0) + ' ' + data[0].priceCurrency;
+        }));
 }
